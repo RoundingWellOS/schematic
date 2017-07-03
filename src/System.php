ab19b268a5a8c50d23dc3c465aa8fe442f658d4d
Woody Gilk <woody.gilk@roundingwell.com>
2017-07-03 15:42:12 -0500

    Create missing directories when writing files
    Ensure that the directory exists before writing to it.

--- src/System.php
+++ src/System.php
@@ -10,6 +10,12 @@ class System
 {
     public function writeFile(string $file, string $contents): int
     {
+        $directory = pathinfo($file, PATHINFO_DIRECTORY);
+
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755, true);
+        }
+
         return (int) file_put_contents($file, $contents);
     }
 }
